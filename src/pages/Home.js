8c2aee8e7aed61f1dc89a56fb744bd8118d5a6ca
nathanvr <jonathanvaro1004@gmail.com>
2022-12-22 22:18:51 -0500

    adds the infinite scroll feature using infinite scroll component library

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,51 @@
 import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { Link } from "react-router-dom";
 import CharactersCard from "../components/CharactersCard";
 
 const Home = () => {
-  const [characters, setCharacters] = useState(null);
+  const [data, setData] = useState({ info: {}, results: [] });
+  const [page, setPage] = useState(1);
+
+  // const character = [];
 
   useEffect(() => {
-    fetch("https://rickandmortyapi.com/api/character")
+    fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
       .then((response) => response.json())
-      .then((data) => setCharacters(data.results));
-  }, []);
 
-  if (!characters) {
+      .then((data) =>
+        setData((prevData) => {
+          return {
+            ...prevData,
+            ...data,
+            results: [...prevData.results, ...data.results],
+          };
+        })
+      );
+  }, [page]);
+
+  if (!data) {
     return <>loading...</>;
   }
 
   return (
     <div className="home">
       <div className="home__title">
-        <h1>Lista de personajes</h1>
+        <h1>Lista de personajes </h1>
       </div>
       <div className="home__character__card">
-        {characters.map((character) => (
-          <Link to={`character/${character.id}`} key={character.id}>
-            <CharactersCard character={character} />
-          </Link>
-        ))}
+        <InfiniteScroll
+          dataLength={page * 20}
+          next={() => setPage((prevPage) => prevPage + 1)}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+          {data.results.map((character) => (
+            <Link to={`character/${character.id}`} key={character.id}>
+              <CharactersCard character={character} />
+            </Link>
+          ))}
+        </InfiniteScroll>
       </div>
     </div>
   );
